ba798b271dbfaa2a9d4f8519ac5ec1c0f43b97ca
PancreasMonster <ryanflood444@gmail.com>
2018-12-17 15:33:01 +0000

    Perlin Noise

---
+++ Assets/Scripts/PerlinNoiseSetter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinNoiseSetter : MonoBehaviour {
+
+    // "Bobbing" animation from 1D Perlin noise.
+
+    // Range over which height varies.
+    float heightScale = 15.0f;
+
+    // Distance covered per second along X axis of Perlin plane.
+    float xScale;
+
+    void Start()
+    {
+        xScale = Random.Range(0, 15);
+        float height = Mathf.PerlinNoise(transform.position.x * xScale, 0.0f) + Mathf.PerlinNoise(transform.position.x * xScale, 0.0f);
+        Vector3 pos = transform.position;
+        pos.y = height;
+        transform.position = pos;
+    }
+
+    float SampleCell1(float x, float y)
+    {
+        return (
+         Mathf.PerlinNoise(10000 + x / 100, 10000 + y / 100) * 100)
+         + (Mathf.PerlinNoise(10000 + x / 1000, 10000 + y / 1000) * 300)
+         + (Mathf.PerlinNoise(1000 + x / 5, 100 + y / 5) * 2);
+    }
+
+
+    void Update()
+    {
+        float height = Mathf.PerlinNoise(transform.position.x * xScale, 0.0f) + Mathf.PerlinNoise(transform.position.x * xScale, 0.0f);   //uses perlin noise to set the waypoint heights
+        Vector3 pos = transform.position;
+        pos.y = height;
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public float wpNum, gapSize;
+    public GameObject waypoint;
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < wpNum; i++)
+        {
+            GameObject Clone = Instantiate(waypoint, new Vector3(i * gapSize, 0, 0), Quaternion.identity);
+            Clone.name = "Waypoint " + i;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
