f1cc62f5fdefa2d9e8e93d9f608c3efd27f3e254
PancreasMonster <ryanflood444@gmail.com>
2018-12-19 00:44:57 +0000

    LerpingCamera

---
+++ Assets/Scripts/CameraLerp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLerp : MonoBehaviour {
+
+    public Transform target, lookAtTarget;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 targetPos = lookAtTarget.position - transform.position;
+
+        transform.position = Vector3.Lerp(transform.position, target.position, speed * Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetPos), speed * Time.deltaTime);
+	}
+}
