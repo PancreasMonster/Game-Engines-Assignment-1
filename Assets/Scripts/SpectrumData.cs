483dfd70cf0a4a94a28ce08bce86b0141507646d
PancreasMonster <ryanflood444@gmail.com>
2018-12-19 20:15:09 +0000

    Getting Spectrum Data

--- Assets/Scripts/SpectrumData.cs
+++ Assets/Scripts/SpectrumData.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class SpectrumData : MonoBehaviour {
 
     AudioSource aud;
+    public float[] audSamples = new float[512];
+
 	// Use this for initialization
 	void Start () {
-		
+        aud = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        GetSpectrum();
 	}
+
+    void GetSpectrum()
+    {
+        aud.GetSpectrumData(audSamples, 0, FFTWindow.Blackman);
+    }
 }
